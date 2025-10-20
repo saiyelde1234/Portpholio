b0d995210f02c6362bab4fe59f3b65549b9603e8
Saiprasad Yelde <saiyelde123@gmail.com>
2025-10-20 08:46:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text",{
+    strings: ["Mern Developer","Web Developer","Software Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
